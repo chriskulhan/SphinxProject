c54fd1559e18e7f5726cf5e66d27ea87f36453ce
chriskulhan <156239549+chriskulhan@users.noreply.github.com>
2025-04-06 13:35:30 -0500

    change conf.py to different html style

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ThisIsASphinxProject'
+project = 'This Is A Sphinx Project'
 copyright = '2025, chris kulhanek'
 author = 'chris kulhanek'
 release = '2025'
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
